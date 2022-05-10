758e491550691ca47127f2be672e100dd4bbbb7f
Kerim <bekmuradov1998@mail.ru>
2022-05-11 00:07:54 +0300

    Corrected typo

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -4,8 +4,8 @@ void SortingElementsOfArray( string [] arrayForSorting)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        //int m = 3;
-        if (array[i].Length <= 3)
+        int m = 3;
+        if (array[i].Length <= m)
         {
             Console.WriteLine($"{array[i]}");
         }    

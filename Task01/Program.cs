22964cbb27ba6e8d6b97558bdb127a38738fb732
Kerim <bekmuradov1998@mail.ru>
2022-05-10 23:20:44 +0300

    Written programm

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] array = {"hello","2","world",":-)"};
+
+void SortingElementsOfArray( string [] arrayForSorting)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        //int m = 3;
+        if (array[i].Length <= 3)
+        {
+            Console.WriteLine($"{array[i]}");
+        }    
+    }
+}
+
+SortingElementsOfArray(array);
